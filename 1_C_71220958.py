786a18ca77050e3c7cb7ceebbc2829f3b9c9d496
KevinGloryPrasetyo-71220958 <117893068+KevinGloryPrasetyo-71220958@users.noreply.github.com>
2022-11-17 16:30:16 +0700

    Add files via upload

---
+++ 1_C_71220958.py
@@ -0,0 +1,9 @@
+nama =str(input("Cassandra Charlotte"))
+namabelakang = "Charlotte"
+matakuliah = "Logika Matematika"
+grup = "A"
+print ("masukan nama anda : Cassandra Charlotte")
+print ("masukan mata kuliah : Logika Matematika")
+print ("masukan grup :A")
+print ("Hallo", namabelakang)
+print (namabelakang, "tergabung dalam kelas", matakuliah, "pada grup", grup)
